aa7e5fd69099e7ba1bccae54815db428b84bd826
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2023-11-20 17:19:25 -0500

    Created the createProduct() method in the ProductController class.

---
+++ src/main/java/com/appsdeveloperblog/ws/products/rest/ProductController.java
@@ -0,0 +1,19 @@
+package com.appsdeveloperblog.ws.products.rest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products") //http://localhost:<port>/products
+public class ProductController {
+	
+	@PostMapping
+	public ResponseEntity<String> createProduct(@RequestBody CreateProductRestModel product) {
+		return ResponseEntity.status(HttpStatus.CREATED).body("");
+	}
+
+}
