4301d32b5ce7f6f636b073e5cda9761aab31ff06
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2023-11-21 10:47:35 -0500

    Create ProductService and inject it to ProductController.

--- src/main/java/com/appsdeveloperblog/ws/products/rest/ProductController.java
+++ src/main/java/com/appsdeveloperblog/ws/products/rest/ProductController.java
@@ -7,13 +7,24 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.appsdeveloperblog.ws.products.service.ProductService;
+
 @RestController
 @RequestMapping("/products") //http://localhost:<port>/products
 public class ProductController {
 	
+	ProductService productService;
+	
+	public ProductController(ProductService productService) {
+		this.productService = productService;
+	}
+	
 	@PostMapping
 	public ResponseEntity<String> createProduct(@RequestBody CreateProductRestModel product) {
-		return ResponseEntity.status(HttpStatus.CREATED).body("");
+		
+		String productId = productService.createProduct(product);
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(productId);
 	}
 
 }
---
+++ src/main/java/com/appsdeveloperblog/ws/products/service/ProductServiceImpl.java
@@ -0,0 +1,16 @@
+package com.appsdeveloperblog.ws.products.service;
+
+import org.springframework.stereotype.Service;
+
+import com.appsdeveloperblog.ws.products.rest.CreateProductRestModel;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+	@Override
+	public String createProduct(CreateProductRestModel productRestModel) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
