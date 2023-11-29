2f4cde13506137b9eb01b9d016f60dfc4485366d
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2023-11-28 19:30:20 -0500

    Send message syncronousely

--- src/main/java/com/appsdeveloperblog/ws/products/service/ProductService.java
+++ src/main/java/com/appsdeveloperblog/ws/products/service/ProductService.java
@@ -4,6 +4,6 @@ import com.appsdeveloperblog.ws.products.rest.CreateProductRestModel;
 
 public interface ProductService {
 	
-	String createProduct(CreateProductRestModel productRestModel);
+	String createProduct(CreateProductRestModel productRestModel) throws Exception ;
 
 }
--- src/main/java/com/appsdeveloperblog/ws/products/service/ProductServiceImpl.java
+++ src/main/java/com/appsdeveloperblog/ws/products/service/ProductServiceImpl.java
@@ -22,7 +22,7 @@ public class ProductServiceImpl implements ProductService {
 	}
 
 	@Override
-	public String createProduct(CreateProductRestModel productRestModel) {
+	public String createProduct(CreateProductRestModel productRestModel) throws Exception {
 		
 		String productId = UUID.randomUUID().toString();
 		
@@ -32,18 +32,8 @@ public class ProductServiceImpl implements ProductService {
 				productRestModel.getTitle(), productRestModel.getPrice(), 
 				productRestModel.getQuantity());
 		
-		CompletableFuture<SendResult<String, ProductCreatedEvent>> future = 
-				kafkaTemplate.send("product-created-events-topic",productId, productCreatedEvent);
-		
-		future.whenComplete((result, exception) -> {
-			
-			if(exception != null) {
-				LOGGER.error("****** Failed to send message: " + exception.getMessage());
-			} else {
-				LOGGER.info("******* Message sent successfully: " + result.getRecordMetadata());
-			}
-			
-		});
+		SendResult<String, ProductCreatedEvent> result = 
+				kafkaTemplate.send("product-created-events-topic",productId, productCreatedEvent).get();
 		
 		LOGGER.info("***** Returning product id");
 		
