f07ce14a8182f8af96cd14a90fc0462e0dfba82c
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2023-11-28 19:30:39 -0500

    Handle Exception in the controller class

--- src/main/java/com/appsdeveloperblog/ws/products/rest/ProductController.java
+++ src/main/java/com/appsdeveloperblog/ws/products/rest/ProductController.java
@@ -1,5 +1,9 @@
 package com.appsdeveloperblog.ws.products.rest;
 
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,15 +18,24 @@ import com.appsdeveloperblog.ws.products.service.ProductService;
 public class ProductController {
 	
 	ProductService productService;
+	private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
 	
 	public ProductController(ProductService productService) {
 		this.productService = productService;
 	}
 	
 	@PostMapping
-	public ResponseEntity<String> createProduct(@RequestBody CreateProductRestModel product) {
+	public ResponseEntity<Object> createProduct(@RequestBody CreateProductRestModel product) {
 		
-		String productId = productService.createProduct(product);
+		String productId;
+		try { 
+			productId = productService.createProduct(product);
+		} catch (Exception e) {
+			//e.printStackTrace();
+			LOGGER.error(e.getMessage(), e);
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+					.body(new ErrorMessage(new Date(), e.getMessage(),"/products"));
+		}
 		
 		return ResponseEntity.status(HttpStatus.CREATED).body(productId);
 	}
