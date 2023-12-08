4f7319fadabe446432b20614f6b1707ecfed6c7d
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2023-12-08 17:36:26 -0500

    Moved ProductCreatedEvent to the "core" project and added maven dependency

--- src/main/java/com/appsdeveloperblog/ws/products/KafkaConfig.java
+++ src/main/java/com/appsdeveloperblog/ws/products/KafkaConfig.java
@@ -12,8 +12,7 @@ import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
-
-import com.appsdeveloperblog.ws.products.service.ProductCreatedEvent;
+import com.appsdeveloperblog.ws.core.ProductCreatedEvent;
 
 @Configuration
 public class KafkaConfig {
