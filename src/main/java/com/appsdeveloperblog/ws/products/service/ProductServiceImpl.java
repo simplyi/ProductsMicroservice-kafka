66d8b3e1f2be920da37933754f9a36774f3c2a1e
simplyi <sergey.kargopolov@gmail.com>
2024-01-26 11:12:44 -0500

    Kafka Producer Bean configuration

--- src/main/java/com/appsdeveloperblog/ws/products/service/ProductServiceImpl.java
+++ src/main/java/com/appsdeveloperblog/ws/products/service/ProductServiceImpl.java
@@ -1,15 +1,11 @@
 package com.appsdeveloperblog.ws.products.service;
 
 import java.util.UUID;
-import java.util.concurrent.CompletableFuture;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
-
-import com.appsdeveloperblog.ws.core.ProductCreatedEvent;
 import com.appsdeveloperblog.ws.products.rest.CreateProductRestModel;
 
 @Service
